1216489504fc856d493072b4e8f559989f17e4d9
chenarmy <chenrmy.cool@163.com>
2016-09-12 16:42:02 +0800

    还原city

--- mobile/city.aspx.cs
+++ mobile/city.aspx.cs
@@ -14,13 +14,12 @@ public partial class mobile_city : System.Web.UI.Page
         if (!IsPostBack)
         {
             string cname = Request.QueryString["cname"];
-            //Response.Write(cname);
             if (cname != null)
             {
                 HttpCookie c1 = new HttpCookie("txlpcity");
                 c1.Value = Server.UrlEncode(cname);
                 Response.Cookies.Add(c1);
-                Page.ClientScript.RegisterStartupScript(GetType(), "", "<script>location.href='index.aspx?citytruename="+cname+"';</script>");
+                Page.ClientScript.RegisterStartupScript(GetType(), "", "<script>location.href='index.aspx';</script>");
             }
             string[] zm = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", };
             string ioliu = "";
@@ -30,11 +29,11 @@ public partial class mobile_city : System.Web.UI.Page
                 ioliu += "<div style='padding:10px; background-color:#e8e8e8; text-align:center; border-bottom:1px solid #d5d5d5;  clear:both;'><span style='font-size:14px; color:#e07211;'><font face='arial'><b>" + zm[i] + "：</b><font></span></div>";
                 if (zm[i] == "C")
                 {
-                    ioliu += "<a href='index.aspx?citytruename=重庆市' style='font-size:1.1em; color:#7724f5; text-decoration:none; float:left; margin:15px;'><font face='微软雅黑'>重庆市</font></a>";
+                    ioliu += "<a href='city.aspx?cname=重庆市' style='font-size:1.1em; color:#7724f5; text-decoration:none; float:left; margin:15px;'><font face='微软雅黑'>重庆市</font></a>";
                 }
                 foreach (DataRow dt in dal.select("select * from address_s where dbo.f_GetPy(sname) like '" + zm[i] + "%' order by spai desc").Rows)
                 {
-                    ioliu += "<a href='index.aspx?citytruename=" + dt["sname"].ToString() + "' style='font-size:1.1em; color:#7724f5; text-decoration:none; float:left; margin:15px;'><font face='微软雅黑'>" + dt["sname"].ToString() + "</font></a>";
+                    ioliu += "<a href='city.aspx?cname=" + dt["sname"].ToString() + "' style='font-size:1.1em; color:#7724f5; text-decoration:none; float:left; margin:15px;'><font face='微软雅黑'>" + dt["sname"].ToString() + "</font></a>";
                 }
             }
             string aname = Request.QueryString["aname"];
@@ -43,11 +42,11 @@ public partial class mobile_city : System.Web.UI.Page
                 ioliu = "";
                 if (aname == "C")
                 {
-                    ioliu += "<a href='index.aspx?citytruename=重庆市' style='font-size:1.1em; color:#7724f5; text-decoration:none; float:left; margin:15px;'><font face='微软雅黑'>重庆市</font></a>";
+                    ioliu += "<a href='city.aspx?cname=重庆市' style='font-size:1.1em; color:#7724f5; text-decoration:none; float:left; margin:15px;'><font face='微软雅黑'>重庆市</font></a>";
                 }
                 foreach (DataRow dt in dal.select("select * from address_s where dbo.f_GetPy(sname) like '" + aname + "%' order by spai desc").Rows)
                 {
-                    ioliu += "<a href='index.aspx?citytruename=" + dt["sname"].ToString() + "' style='font-size:1.1em; color:#7724f5; text-decoration:none; float:left; margin:15px;'><font face='微软雅黑'>" + dt["sname"].ToString() + "</font></a>";
+                    ioliu += "<a href='city.aspx?cname=" + dt["sname"].ToString() + "' style='font-size:1.1em; color:#7724f5; text-decoration:none; float:left; margin:15px;'><font face='微软雅黑'>" + dt["sname"].ToString() + "</font></a>";
                 }
             }
             this.Label1.Text = ioliu;
